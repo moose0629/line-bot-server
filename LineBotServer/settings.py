154981feb1933a173d23580b9e9ecda8a4a493d2
MooseChen <moosechen@wistronits.com>
2022-02-03 23:03:45 +0800

    modify DEBUG mode for production

--- LineBotServer/settings.py
+++ LineBotServer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4v(2v3n8ht2#6+p04-vkr-4sp_oulj7!zank=$b0^t0yp+&!om'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
