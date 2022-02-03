36b945ff2ea7961fb138a867e2ff1030cb607f4e
MooseChen <moosechen@wistronits.com>
2022-02-03 18:30:03 +0800

    <feat>新增跳過夜訓 和變更夜訓主題的方法, 調整週次說明文字

--- messagelinebot/funcs.py
+++ messagelinebot/funcs.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import pygsheets
 import json
 import os
@@ -5,18 +6,54 @@ import os
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 fileName = os.path.join(fileDir, 'docs\\firstfirebase-f6e15-dcc94b5fa7ed.json')
 
+# 檢查當日是否為週二，且為當月最後一週
 
-def reverse_week_type():
 
+def check_is_last_tuesday():
+    now = datetime.now()
+    today_plus_7_day = now + timedelta(days=7)
+    return now.isoweekday == 2 and now.month != today_plus_7_day.month
+
+# 調整夜訓的主題設定
+
+
+def reverse_week_type(iscron=True):
     gc = pygsheets.authorize(
         service_account_file=fileName)
     web_url = "https://docs.google.com/spreadsheets/d/1v0G4cp02Qq4zM7miulrqixL0tbR5v_yoASRdoTa5I9U"
     wb_url = gc.open_by_url(web_url)
     sh = wb_url.sheet1
+    result_row = sh.get_row(2)
+    # 如果是排程執行
+    if iscron:
+        # 如果該週不為最後一個週二
+        if not check_is_last_tuesday():
+            # 該週沒有跳過在執行
+            if result_row[1] != "TRUE":
+                result = result_row[0] == "TRUE"
+                sh.cell("A2").value = not result
+                return not result
+            # 如果有跳過，就將pass 設定回 false
+            else:
+                sh.cell("B2").value = False
+                return True
+    else:
+        result = result_row[0] == "TRUE"
+        sh.cell("A2").value = not result
+        return not result
 
-    result = json.loads(sh.cell("A1").value)
-    result['is_recruit_week'] = not result['is_recruit_week']
-    sh.cell("A1").value = json.dumps(result)
+# 跳過該週
+
+
+def pass_this_week():
+    gc = pygsheets.authorize(
+        service_account_file=fileName)
+    web_url = "https://docs.google.com/spreadsheets/d/1v0G4cp02Qq4zM7miulrqixL0tbR5v_yoASRdoTa5I9U"
+    wb_url = gc.open_by_url(web_url)
+    sh = wb_url.sheet1
+    sh.cell("B2").value = True
+
+# 判斷該週的主題內容是什麼
 
 
 def get_week_type():
@@ -26,4 +63,4 @@ def get_week_type():
     wb_url = gc.open_by_url(web_url)
     sh = wb_url.sheet1
 
-    return json.loads(sh.cell("A1").value)['is_recruit_week']
+    return sh.cell("A2").value == "TRUE"
